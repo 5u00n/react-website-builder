c0ffe686954ef1a55f08a946087c37750ca405e6
5u00n <surenhembram97@gmail.com>
2024-10-28 03:15:21 +0530

    Refactor project dependencies and remove unnecessary build files

--- src/components/Editor/ViewPort/Header.jsx
+++ src/components/Editor/ViewPort/Header.jsx
@@ -2,22 +2,32 @@ import { useEditor } from '@craftjs/core';
 import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
 
 import cx from 'classnames';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { CheckIcon, ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons';
+import { CheckIcon, ArrowLeftIcon, ArrowRightIcon, LockClosedIcon } from '@radix-ui/react-icons';
 import { MdBuild } from 'react-icons/md';
 
 export const Header = () => {
-  const { enabled, canUndo, canRedo, actions } = useEditor((state, query) => ({
+  const { enabled, canUndo, canRedo, actions, query } = useEditor((state, query) => ({
     enabled: state.options.enabled,
     canUndo: query.history.canUndo(),
     canRedo: query.history.canRedo(),
   }));
 
-  console.log(enabled);
+  const saveState = () => {
+    const data = JSON.stringify(query.serialize());
+    localStorage.setItem('craft.js.data', data);
+  }
+
+  useEffect(() => {
+    const data = localStorage.getItem('craft.js.data');
+    if (data) {
+      actions.deserialize(JSON.parse(data));
+    }
+  }, [actions]);
 
   return (
-    <div className="w-full h-11 z-[99999] relative px-2.5 bg-gray-400 flex text-white transition">
+    <div className={`w-full h-11 z-[99999] relative px-2.5 bg-gray-400 flex text-white transition`}>
       <div className="items-center flex w-full px-4 justify-end">
         {enabled && (
           <div className="flex-1 flex space-x-2.5">
@@ -43,6 +53,20 @@ export const Header = () => {
               </TooltipTrigger>
               <TooltipContent>Redo</TooltipContent>
             </Tooltip>
+
+            <div className="flex items-center space-x-2.5">
+              <Tooltip>
+                <TooltipTrigger>
+                  <a className="cursor-pointer flex rounded bg-green-500 px-3 py-1 hover:bg-green-800"
+                    onClick={saveState}
+                  >
+                    <LockClosedIcon className="w-5 h-6 fill-gray-500 mr-2" />
+                    <span className="text-md">Save</span>
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent>Save</TooltipContent>
+              </Tooltip>
+            </div>
           </div>
         )}
         <div className="flex">
