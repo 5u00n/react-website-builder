93e1135bcdae957ae0888a53762362c432a8f80a
5u00n <surenhembram97@gmail.com>
2024-11-07 18:16:51 +0530

    Refactor Toolbox component to use TooltipElement for cleaner code and improved readability

--- src/components/Editor/ViewPort/Toolbox.jsx
+++ src/components/Editor/ViewPort/Toolbox.jsx
@@ -10,6 +10,21 @@ import { Video } from '../../Elements/Video';
 import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
 import { ResizablePanel } from './../ResizablePanel';
 
+
+const TooltipElement = ({ icon, text }) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <a className="m-1 cursor-move flex flex-col items-center justify-center border border-gray-200 rounded p-1 shadow-sm w-[56px] h-[56px]">
+          {icon}
+          <span className=' text-xs'>{text}</span>
+        </a>
+      </TooltipTrigger>
+      <TooltipContent>{text}</TooltipContent>
+    </Tooltip>
+  );
+};
+
 export const Toolbox = () => {
   const {
     enabled,
@@ -25,75 +40,43 @@ export const Toolbox = () => {
 
   return (
     <ResizablePanel defaultWidth={90} minWidth={90} maxWidth={400}>
-    <div className={`h-full overflow-auto p-1`}>
-      <div className={`toolbox transition w-full h-full flex flex-col bg-white op border border-gray-200`}>
-        <h6 className='text-center text-xs font-bold py-3'>Elements</h6>
-        <div className="flex flex-wrap items-center justify-center">
-          <div
-            ref={(ref) =>
-              create(
-                ref,
-                <Element
-                  canvas
-                  is={Container}
-                  background={{ r: 78, g: 78, b: 78, a: 1 }}
-                  color={{ r: 0, g: 0, b: 0, a: 1 }}
-                  height="300px"
-                  width="100%"
-                ></Element>
-              )
-            }
-          >
-            <Tooltip>
-              <TooltipTrigger>
-                <a className="m-1 cursor-move flex flex-col items-center justify-center border border-gray-200 rounded p-1 shadow-sm w-[56px] h-[56px]">
-                  <FaRegSquare className="w-6 h-6 fill-gray-500" />
-                  <span className=' text-xs'>Container</span>
-                </a>
-              </TooltipTrigger>
-              <TooltipContent>Container</TooltipContent>
-            </Tooltip>
-          </div>
-          <div
-            ref={(ref) =>
-              create(ref, <Text fontSize="12" textAlign="left" text="Hi there" />)
-            }
-          >
-            <Tooltip>
-              <TooltipTrigger>
-                <a className="m-1 cursor-move flex flex-col items-center justify-center border border-gray-200 rounded p-1 shadow-sm w-[56px] h-[56px]">
-                  <FaFont className="w-6 h-6 fill-gray-500" />
-                  <span className=' text-xs'>Text</span>
-                </a>
-              </TooltipTrigger>
-              <TooltipContent>Text</TooltipContent>
-            </Tooltip>
-          </div>
-          <div ref={(ref) => create(ref, <Button />)}>
-            <Tooltip>
-              <TooltipTrigger>
-                <a className="m-1 cursor-move flex flex-col items-center justify-center border border-gray-200 rounded p-1 shadow-sm w-[56px] h-[56px]">
-                  <FaRegHandPointer className="w-6 h-6 fill-gray-500" />
-                  <span className=' text-xs'>Button</span>
-                </a>
-              </TooltipTrigger>
-              <TooltipContent>Button</TooltipContent>
-            </Tooltip>
-          </div>
-          <div ref={(ref) => create(ref, <Video />)}>
-            <Tooltip>
-              <TooltipTrigger>
-                <a className="m-1 cursor-move flex flex-col items-center justify-center border border-gray-200 rounded p-1 shadow-sm w-[56px] h-[56px]">
-                  <FaYoutube className="w-6 h-6 fill-gray-500" />
-                  <span className=' text-xs'>Video</span>
-                </a>
-              </TooltipTrigger>
-              <TooltipContent>Video</TooltipContent>
-            </Tooltip>
+      <div className={`h-full overflow-auto p-1`}>
+        <div className={`toolbox transition w-full h-full flex flex-col bg-white op border border-gray-200`}>
+          <h6 className='text-center text-xs font-bold py-3'>Elements</h6>
+          <div className="flex flex-wrap items-center justify-center">
+            <div
+              ref={(ref) =>
+                create(
+                  ref,
+                  <Element
+                    canvas
+                    is={Container}
+                    background={{ r: 78, g: 78, b: 78, a: 1 }}
+                    color={{ r: 0, g: 0, b: 0, a: 1 }}
+                    height="300px"
+                    width="100%"
+                  ></Element>
+                )
+              }
+            >
+              <TooltipElement icon={<FaRegSquare className="w-6 h-6 fill-gray-500" />} text="Container" />
+            </div>
+            <div
+              ref={(ref) =>
+                create(ref, <Text fontSize="12" textAlign="left" text="Hi there" />)
+              }
+            >
+              <TooltipElement icon={<FaFont className="w-6 h-6 fill-gray-500" />} text="Text" />
+            </div>
+            <div ref={(ref) => create(ref, <Button />)}>
+              <TooltipElement icon={<FaRegHandPointer className="w-6 h-6 fill-gray-500" />} text="Button" />
+            </div>
+            <div ref={(ref) => create(ref, <Video />)}>
+              <TooltipElement icon={<FaYoutube className="w-6 h-6 fill-gray-500" />} text="Video" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </ResizablePanel>
   );
 };
